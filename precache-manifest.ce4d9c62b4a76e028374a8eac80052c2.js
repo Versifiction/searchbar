62d776ca098b67b332af0b290a7a300268db65dd
Marc Charpentier <marc_charpentier@hotmail.fr>
2019-01-21 15:13:41 +0100

    Updates

---
+++ precache-manifest.ce4d9c62b4a76e028374a8eac80052c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f375f8f408b5c78d9aaa26e3f6a5e96a",
+    "url": "/searchbar/static/media/female.f375f8f4.png"
+  },
+  {
+    "revision": "927b49ef74111b7df0cb",
+    "url": "/searchbar/static/js/runtime~main.927b49ef.js"
+  },
+  {
+    "revision": "a4276b53d1f567eda740",
+    "url": "/searchbar/static/js/main.a4276b53.chunk.js"
+  },
+  {
+    "revision": "6009fa94a37e0de1b354",
+    "url": "/searchbar/static/js/1.6009fa94.chunk.js"
+  },
+  {
+    "revision": "a4276b53d1f567eda740",
+    "url": "/searchbar/static/css/main.423d782b.chunk.css"
+  },
+  {
+    "revision": "cb44d36a7392e8e3e07db41310a43e56",
+    "url": "/searchbar/index.html"
+  }
+];
